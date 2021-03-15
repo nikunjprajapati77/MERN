5e27489f0b02f40657e206223646457dbd82ba3d
nikunj-tmedia <nprajapati@tmediabusinesssolution.com>
2021-03-15 15:14:33 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,7 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <Typography className={classes.heading} variant="h2" align="center">MERN (Mongodb, Express, ReactJs, NodeJs)</Typography>
       </AppBar>
       <Grow in>
         <Container>
